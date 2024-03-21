44bf22da0af9e3336a8c3db46f18bbf8712674b0
ricardofig016 <ricardifigueiredo016@gmail.com>
2024-03-21 12:00:13 +0000

    aes decryption

--- tp1/aes.py
+++ tp1/aes.py
@@ -1,11 +1,9 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import unpad
 
 
-def aes_encrypt(plaintext, key):
-    if len(key) != 32:
-        raise ValueError("Key must be 32 bytes for AES-256.")
-
+def encrypt(plaintext, key):
     iv = get_random_bytes(AES.block_size)
 
     cipher = AES.new(key, AES.MODE_CBC, iv)
@@ -18,3 +16,14 @@ def aes_encrypt(plaintext, key):
     ciphertext = cipher.encrypt(plaintext)
 
     return iv + ciphertext
+
+
+def decrypt(ciphertext, key):
+    iv = ciphertext[: AES.block_size]
+    ciphertext = ciphertext[AES.block_size :]
+
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+
+    return plaintext
--- tp1/main.py
+++ tp1/main.py
@@ -10,17 +10,12 @@ from files import (
     RSA_SIZES,
     BASE_FILE_PATH as BASE_RANDOM_FILES_PATH,
 )
-from aes import aes_encrypt
+from aes import encrypt as aes_encrypt, decrypt as aes_decrypt
+
 from icecream import ic
 
 
-if __name__ == "__main__":
-    gen_files()
-
-    encryption_times = {}  # in seconds
-    decryption_times = {}  # in seconds
-
-    # AES
+def measure_aes():
     for size in AES_SIZES:
         file_path = os.path.join(BASE_RANDOM_FILES_PATH, f"aes_{size}.txt")
         with open(file_path, "r") as file:
@@ -36,6 +31,24 @@ if __name__ == "__main__":
         # decryption
         key = get_random_bytes(32)
         encrypted_data = aes_encrypt(plaintext, key)
-        # to implement...
+
+        timer = Timer(lambda: aes_decrypt(encrypted_data, key))
+        iterations = 100
+        time_elapsed = timer.timeit(number=iterations)
+        average_time_elapsed = time_elapsed / iterations
+        decryption_times[f"aes_{size}"] = average_time_elapsed
+
+        # verification
+        # print(aes_decrypt(encrypted_data, key).decode("utf-8") == plaintext)
+
+
+if __name__ == "__main__":
+    gen_files()
+
+    encryption_times = {}  # in seconds
+    decryption_times = {}  # in seconds
+
+    measure_aes()
 
     ic(encryption_times)
+    ic(decryption_times)
